95ea4b35ac0888540d9c1494742a6a21f75911ce
seod0209 <don9zseo@gmail.com>
2024-07-10 15:56:05 +0900

    fix: enable esModuleInterop and update p5 import in index.ts
    - Set "esModuleInterop": true in tsconfig.json to handle ES module interop.
    - Changed import statement from 'import p5 from 'p5';' to 'import * as p5 from 'p5';' to correctly import p5 as a module.
    - Updated instantiation of p5 instance to 'new p5.default(sketch);' to resolve TypeScript type errors.

--- src/star.ts
+++ src/star.ts
@@ -1,15 +1,19 @@
-import P5 from "p5";
+import * as P5 from "p5";
 
 class Star {
   p5: P5;
   x: number;
   y: number;
+  originX: number;
+  originY: number;
   size: number;
   alpha: number;
   constructor(p5: P5, width: number, height: number) {
     this.p5 = p5;
     this.x = this.random(1, width);
     this.y = this.random(1, height);
+    this.originX = this.x;
+    this.originY = this.y;
     this.size = this.random(3, 5);
     this.alpha = this.random(100, 255);
   }
@@ -21,6 +25,20 @@ class Star {
   show() {
     this.p5.fill(255, this.alpha);
     this.p5.ellipse(this.x, this.y, this.size, this.size);
+  }
+
+  updatePosition(x: number, y: number) {
+    let d = this.p5.dist(x, y, this.x, this.y);
+
+    // 반경 50px 내에 별을 그리지 않는다
+    if (d < 50) {
+      this.x = x + 50;
+      this.y = x + 50;
+    } else {
+      // 원상복구
+      this.x = this.originX;
+      this.y = this.originY;
+    }
   }
 }
 
