2407ed3058ac34b1ef55236adecd743a562e0b5b
seod0209 <don9zseo@gmail.com>
2024-07-17 21:43:41 +0900

    feat: change circle to sqaure
    style: remove margin in body tag

--- src/whale.ts
+++ src/whale.ts
@@ -1,6 +1,9 @@
 import * as p5 from "p5";
 import Fin from "./fin";
 
+let segSizes = [50, 70, 65, 20, 18, 6, 6, 3].reverse();
+let marginSizes = [2, 2, 2, 2, 2];
+
 export default class Whale {
   p5: p5;
 
@@ -16,6 +19,8 @@ export default class Whale {
 
   fins: Fin[];
 
+  numFin: number;
+
   boundary: number;
 
   constructor(p5: p5) {
@@ -29,6 +34,8 @@ export default class Whale {
 
     this.fins = [new Fin(this.p5, this.wPos.x, this.wPos.y, this._size)];
 
+    this.numFin = 4;
+
     this.boundary = 10;
   }
 
@@ -38,7 +45,7 @@ export default class Whale {
   }
 
   getFins() {
-    for (let i = 1; i < 4; i++) {
+    for (let i = 1; i < this.numFin; i++) {
       const { p5, pos, w } = this.fins[i - 1];
       const prevSize = i === 1 ? 0 : w;
 
@@ -48,6 +55,18 @@ export default class Whale {
     }
 
     this.fins.reverse();
+    this.setSizes();
+  }
+
+  setSizes() {
+    // set sizes for segments
+    let trapCounter = 0;
+    for (let i = this.numFin - 1; i >= 0; i--) {
+      this.fins[i].setMargin(marginSizes[i]);
+
+      this.fins[i].setFrontW(segSizes[trapCounter++]);
+      this.fins[i].setBackW(segSizes[trapCounter++]);
+    }
   }
 
   updatePosition() {
@@ -71,8 +90,8 @@ export default class Whale {
 
     this.fins.forEach((f, i) => {
       if (i > 0) {
-        const frontFin = this.fins[i - 1];
-        f.update(frontFin.pos.x, frontFin.pos.y);
+        const prevFin = this.fins[i - 1];
+        f.update(prevFin.pos.x, prevFin.pos.y);
       } else {
         this.fins[0].update(this.wPos.x, this.wPos.y);
       }
