8cbcccc8c7cb2252d836f89a9f5b835b5eb5db1c
seod0209 <don9zseo@gmail.com>
2024-07-18 11:27:16 +0900

    feat: add square star

--- src/star.ts
+++ src/star.ts
@@ -1,6 +1,9 @@
-import * as P5 from "p5";
+import p5 from "p5";
+import P5 from "p5";
 
 let shieldR = 70;
+let starCenterW = 2;
+let starTriW = 12;
 
 class Star {
   p5: P5;
@@ -10,7 +13,8 @@ class Star {
   originY: number;
   size: number;
   alpha: number;
-  constructor(p5: P5, width: number, height: number) {
+  index: number;
+  constructor(p5: P5, width: number, height: number, index: number) {
     this.p5 = p5;
     this.x = this.random(1, width);
     this.y = this.random(1, height);
@@ -18,6 +22,7 @@ class Star {
     this.originY = this.y; // 초기 y 위치
     this.size = this.random(3, 5);
     this.alpha = this.random(100, 255); // 투명도를 랜덤으로 설정
+    this.index = index;
   }
 
   private random(min: number, max: number) {
@@ -25,8 +30,13 @@ class Star {
   }
 
   show() {
-    this.p5.fill(255, this.alpha);
-    this.p5.ellipse(this.x, this.y, this.size, this.size);
+    this.p5.fill(255);
+
+    if (this.index % 71 === 0) {
+      starVertices(this.p5, this.x, this.y);
+    } else {
+      this.p5.ellipse(this.x, this.y, this.size, this.size);
+    }
   }
 
   updatePosition(x: number, y: number) {
@@ -40,10 +50,10 @@ class Star {
 
     // 반경 50px 내에 별을 그리지 않는다
     if (distance < shieldR) {
-      // 공에서 별로 향하는 벡터의 단위벡터를 구합니다.
+      // 공에서 별로 향하는 벡터의 단위벡터
       let unitX = dx / distance;
       let unitY = dy / distance;
-      // 별의 위치를 공과 반대 방향으로 조정합니다.
+      // 별의 위치를 공과 반대 방향으로 조정
       this.x -= unitX * 2;
       this.y -= unitY * 2;
     } else {
@@ -53,5 +63,51 @@ class Star {
     }
   }
 }
+
+const starVertices = (p5: p5, x: number, y: number) => {
+  // p5.textSize(this.random(30, 40));
+  // p5.text("ㅋ", this.x, this.y);
+
+  // 4각형 별 그리기
+  p5.push();
+  p5.translate(x, y);
+  p5.fill(255);
+  p5.noStroke();
+
+  p5.beginShape();
+
+  // 중심 사각형의 꼭짓점 좌표
+  const vertices = [
+    p5.createVector(-starCenterW, starCenterW),
+    p5.createVector(starCenterW, starCenterW),
+    p5.createVector(starCenterW, -starCenterW),
+    p5.createVector(-starCenterW, -starCenterW),
+  ];
+
+  // 삼각형을 위한 중앙 좌표
+  const middlePoints = [
+    p5.createVector(0, starTriW), // 아래쪽 삼각형
+    p5.createVector(starTriW, 0), // 오른쪽 삼각형
+    p5.createVector(0, -starTriW), // 위쪽 삼각형
+    p5.createVector(-starTriW, 0), // 왼쪽 삼각형
+  ];
+
+  // 첫 번째 꼭짓점
+  p5.vertex(vertices[0].x, vertices[0].y);
+
+  // 각 꼭짓점과 중앙 좌표를 연결
+  for (let i = 0; i < vertices.length; i++) {
+    let nextIndex = (i + 1) % vertices.length;
+    let mid = middlePoints[i];
+    let nextVertex = vertices[nextIndex];
+
+    p5.vertex(mid.x, mid.y);
+    p5.vertex(nextVertex.x, nextVertex.y);
+  }
+
+  p5.endShape(p5.CLOSE);
+
+  p5.pop();
+};
 
 export default Star;
